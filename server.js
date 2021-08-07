3592f98c1c92af63234e3745a5a08f4d61ab0181
Diamond <williams.diamond2392@gmail.com>
2021-08-07 13:01:17 -0400

    updating server

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ http.listen(3000, function (){
 }); 
 
 
+
+// older code
+
 // const express = require('express');
 // const path = require('path');
 // const { ApolloServer } = require('apollo-server-express');
