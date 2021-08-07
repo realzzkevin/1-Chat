00d3fb6f1e52164d5c4e32d6348bbaaa62af1635
Diamond <williams.diamond2392@gmail.com>
2021-08-07 11:18:16 -0400

    added package.json

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const io = require('socket.io')(3000)
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+  
+})
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+   
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+
+
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//   });
+  
+//   server.applyMiddleware({ app });
