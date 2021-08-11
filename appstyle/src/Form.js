04e2a45a35b4d18af3e5d9fc8c3dc85cc4e90c14
Nish Mitchell <shanishamit@gmail.com>
2021-08-11 18:20:10 -0400

    addign react chat files

--- appstyle/src/Form.js
+++ appstyle/src/Form.js
@@ -1,81 +1,71 @@
-import React, { useState } from 'react';
-// Here we import a helper function that will check if the email is valid
-// import { checkPassword, validateEmail } from '../../utils/helpers';
+// import React, { useState } from 'react';
+// // Here we import a helper function that will check if the email is valid
+// // import { checkPassword, validateEmail } from '../../utils/helpers';
 
-function Form() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  const [Name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+// function Form() {
+//   // Create state variables for the fields in the form
+//   // We are also setting their initial values to an empty string
+//   const [message, setMessage] = useState('');
+//   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
+//   const handleInputChange = (e) => {
+//     // Getting the value and name of the input which triggered the change
+//     const { target } = e;
+//     const inputType = target.name;
+//     // const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'Name') {
-      setName(inputValue);
-    } else {
-      setMessage(inputValue);
-    }
-  };
+//     // Based on the input type, we set the state of either email, username, and password
+//     if (inputType === 'message') {
+  
+//     }
+//   };
 
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
+//   const handleFormSubmit = (e) => {
+//     // Preventing the default behavior of the form submit (which is to refresh the page)
+//     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!email || !Name || !message) {
-      setErrorMessage('Fields cannot be empty');
-    //   // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-    }
-    // If everything goes according to plan, we want to clear out the input after a successful contact.
-    setName('');
-    setMessage('');
-    setEmail('');
-  };
+//     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+//     if (!message) {
+//       setErrorMessage('Fields cannot be empty');
+//     //   // We want to exit out of this code block if something is wrong so that the user can correct it
+//       return;
+//     }
+//     // If everything goes according to plan, we want to clear out the input after a successful contact.
+  
+//     setMessage('');
+//   };
 
-  return (
-    <div>
-      <form className="form">
-      <input
-          value={Name}
-          name="Name"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Name"
-        />
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="email"
-        />
-        
-        <input
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="textarea"
-          placeholder="Message"
-        />
-        <button type="button" onClick={handleFormSubmit}>Submit</button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <div className="chat-popup" id="message-container">
+//       <form id="send-container" action="/action_page.php" className="form-container">
+//         <input
+//           value={message}
+//           name="message"
+//           onChange={handleInputChange}
+//           type="textarea"
+//           placeholder="Message"
+//         />
+//         <button type="button" id="send-button" onClick={handleFormSubmit}>Submit</button>
+//       </form>
+//       {errorMessage && (
+//         <div>
+//           <p className="error-text">{errorMessage}</p>
+//         </div>
+//       )}
+//     </div>
+//     </div>
+//   );
+// }
 
-export default Form;
+// export default Form;
+
+// /* <div class="chat-popup" id="message-container">
+//   <form id="send-container" action="/action_page.php" class="form-container">
+//     <h1 style="font-family: 'Merriweather', serif;">1<span style="font-family: 'Marck Script', cursive;">Chat</h1>
+//     <label for="msg"><b></b></label>
+//     <textarea input type="text" id="message-input" placeholder="Type message.." name="msg" required></textarea>
+//     <button type="submit"  class="btn">Send</button>
+//   </form>
+// </div>
+// </div> */
---
+++ appstyle/src/components/Replymessagebox.js
@@ -0,0 +1,11 @@
+<MessageBox
+    reply={{
+        photoURL: 'https://facebook.github.io/react/img/logo.svg',
+        title: 'elit magna',
+        titleColor: '#8717ae',
+        message: 'Aliqua amet incididunt id nostrud',
+    }}
+    onReplyMessageClick={() => console.log('reply clicked!')}
+    position={'left'}
+    type={'text'}
+    text={'Tempor duis do voluptate enim duis velit veniam aute ullamco dolore duis irure.'}/>
